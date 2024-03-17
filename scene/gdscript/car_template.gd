112aa670f8f0bf9c4249a8c55271eb5de3fa9a3f
shahriar <shahriarlabib000@gmail.com>
2024-03-17 23:36:36 +0600

    lol

---
+++ scene/gdscript/car_template.gd
@@ -0,0 +1,38 @@
+extends VehicleBody3D
+
+var max_rpm=5000
+var max_torque=500
+
+@onready var pivot= $pivot
+@onready var frontCam= $pivot/frontCam
+@onready var backCam=$pivot/backCam
+
+var lookAt
+func _ready():
+	lookAt= global_position
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	steering =lerp(steering, Input.get_axis("ui_right","ui_left") * 0.4,5 * delta)
+	
+	var accelaration = Input.get_axis("backward","forward") 
+	var rpm= abs($backleft.get_rpm())
+	$backleft.engine_force= accelaration * max_torque * (1 - rpm/ max_rpm)
+	
+	rpm= abs($backright.get_rpm())
+	$backright.engine_force = accelaration * max_torque *(1 - rpm / max_rpm)
+	
+	pivot.global_position= pivot.global_position.lerp(global_position, delta * 20)
+	pivot.transform=pivot.transform.interpolate_with(transform, delta * 5)
+	#lookAt= lookAt.lerp(global_position + linear_velocity,delta * 5)
+	lookAt= global_position#lookAt.lerp(global_position, delta * 5)
+	frontCam.look_at(lookAt)
+	backCam.look_at(lookAt)
+	_check_camera_switch()
+	
+	
+func _check_camera_switch():
+	if Input.is_action_pressed("backward") && $backright.get_rpm() < 0:
+		backCam.current=true
+	else:
+		frontCam.current=true
+	
---
+++ scene/gdscript/world.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+@warning_ignore("unused_parameter")
+func _process(delta):
+	if Input.is_action_just_pressed("ui_redo"):
+		get_tree().reload_current_scene()
