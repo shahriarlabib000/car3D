2a0752484ef3051471c9b782a281aff2d3cbcb93
shahriarlabib000 <shahriarlabib000@gmail.com>
2025-01-25 15:11:50 +0600

    sound

--- scene/gdscript/car_template.gd
+++ scene/gdscript/car_template.gd
@@ -5,26 +5,28 @@ extends VehicleBody3D
 @onready var pivot = $pivot
 @onready var frontCam = %frontCam
 @onready var backCam = %backCam
+@onready var pitch = $engineSound.pitch_scale
 
 var lookAt
 func _ready():
 	lookAt= global_position
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
 func _physics_process(delta):
 	steering = lerp(steering, Input.get_axis("ui_right","ui_left") * 0.4,5 * delta)
 	
 	var accelaration = Input.get_axis("backward","forward") 
-	#var rpm= abs($backleft.get_rpm())
 	engine_force = accelaration * max_torque
 
 	#camera
-	pivot.global_position= pivot.global_position.lerp(global_position + Vector3(0,3,0), delta * 20)
-	pivot.transform= pivot.transform.interpolate_with(transform, delta * 5)
-	lookAt= lookAt.lerp(global_position + linear_velocity,delta * 5)
-	lookAt= global_position#lookAt.lerp(global_position, delta * 5)
+	pivot.global_position = pivot.global_position.lerp(global_position + Vector3(0,3,0), delta * 20)
+	pivot.transform = pivot.transform.interpolate_with(transform, delta * 5)
+	lookAt = lookAt.lerp(global_position + linear_velocity,delta * 5)
+	lookAt = global_position#lookAt.lerp(global_position, delta * 5)
 	frontCam.look_at(lookAt)
 	backCam.look_at(lookAt)
 	_check_camera_switch()
+	$engineSound.pitch_scale = 0.05 + linear_velocity.length()/100
 	
 	
 func _check_camera_switch():
--- scene/gdscript/world.gd
+++ scene/gdscript/world.gd
@@ -1,12 +1,5 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	%speed.text = str("%.3f" %$"Nissan GTR".linear_velocity.length())
 	if Input.is_action_just_pressed("ui_redo"):
