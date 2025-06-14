3e944154af7b6cfa3d99c84687cc43faf0c20ef9
shahriarlabib000 <shahriarlabib000@gmail.com>
2025-06-14 19:10:28 +0600

    lil improvements

--- scene/gdscript/canvas_layer.gd
+++ scene/gdscript/canvas_layer.gd
@@ -3,9 +3,16 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass # Replace with function bod
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	$Label.text=str(Engine.get_frames_per_second())+"fps"
+
+
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else :
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	
